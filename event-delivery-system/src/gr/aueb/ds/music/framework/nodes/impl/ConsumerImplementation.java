b460a7d73bc1559acca53d47f0227adc7663abeb
Vasileios Bouzas <bill_bouzas@hotmail.com>
2020-03-25 16:31:17 +0200

    Init consumer implementation

--- event-delivery-system/src/gr/aueb/ds/music/framework/nodes/impl/ConsumerImplementation.java
+++ event-delivery-system/src/gr/aueb/ds/music/framework/nodes/impl/ConsumerImplementation.java
@@ -1,20 +1,28 @@
 package gr.aueb.ds.music.framework.nodes.impl;
 
+import gr.aueb.ds.music.framework.commons.SystemExitCodes;
+import gr.aueb.ds.music.framework.helper.LogHelper;
+import gr.aueb.ds.music.framework.helper.NetworkHelper;
+import gr.aueb.ds.music.framework.helper.PropertiesHelper;
+import gr.aueb.ds.music.framework.commons.ConsoleColors;
 import gr.aueb.ds.music.framework.model.NodeDetails;
 import gr.aueb.ds.music.framework.model.dto.ArtistName;
 import gr.aueb.ds.music.framework.model.dto.Value;
 import gr.aueb.ds.music.framework.nodes.api.Broker;
 import gr.aueb.ds.music.framework.nodes.api.Consumer;
+import gr.aueb.ds.music.framework.nodes.api.Node;
 
 import java.io.IOException;
-import java.net.InetAddress;
 import java.net.Socket;
-import java.util.List;
+import java.util.AbstractMap;
+import java.util.Optional;
+import java.util.Scanner;
 
-public class ConsumerImplementation implements Consumer {
+public class ConsumerImplementation extends NodeAbstractImplementation implements Consumer {
 
-    protected NodeDetails nodeDetails;
     protected Socket socket;
+    protected Broker connectedBroker;
+    protected ArtistName artistName;
 
     public ConsumerImplementation() {
         this.nodeDetails = new NodeDetails();
@@ -39,21 +47,28 @@ public class ConsumerImplementation implements Consumer {
 
     @Override
     public void playData(ArtistName artistName, Value value) {
-
+//        Broker appropriateBrokerForArtist = this.
     }
 
     @Override
-    public void init() throws IOException {
-    }
+    public void init() {
+        Scanner scanner = new Scanner(System.in);
 
-    @Override
-    public List<Broker> getBrokers() {
-        return null;
+        String artistName = "";
+        // Init Consumer & Ask continuously for Songs
+        while (!artistName.equals("-1")) {
+            LogHelper.userInputWithColor(ConsoleColors.BLUE_BOLD, "Type the Artist Name you want to listen: ");
+
+            artistName = scanner.nextLine();
+            this.artistName = new ArtistName(artistName);
+
+            this.connect();
+        }
     }
 
     @Override
     public void connect() {
-
+        this.connectToAppropriateBroker();
     }
 
     @Override
@@ -66,12 +81,34 @@ public class ConsumerImplementation implements Consumer {
 
     }
 
-    public NodeDetails getNodeDetails() {
-        return nodeDetails;
+    private void connectToAppropriateBroker() {
+        try {
+            AbstractMap.SimpleEntry<String, Integer> masterBrokerIpPort = this.getMasterBrokerIpPort();
+            this.socket = NetworkHelper.initConnection(masterBrokerIpPort.getKey(), masterBrokerIpPort.getValue());
+
+        }
+        catch (Exception ex) {
+            LogHelper.error(this,
+                    String.format(PropertiesHelper.getProperty("consumer.node.broker.connection.failed"),
+                            this.nodeDetails.getName()));
+            System.exit(SystemExitCodes.MASTER_NOT_FOUND_ERROR.getCode());
+        }
     }
 
-    public void setNodeDetails(NodeDetails nodeDetails) {
-        this.nodeDetails = nodeDetails;
+    private AbstractMap.SimpleEntry<String, Integer> getMasterBrokerIpPort() {
+        Broker masterBroker = this.getMasterBroker();
+
+        String masterBrokerIp = Optional.ofNullable(masterBroker)
+                .map(Node::getNodeDetails)
+                .map(NodeDetails::getIpAddress)
+                .orElse(PropertiesHelper.getProperty("master.broker.ip"));
+
+        int masterBrokerPort = Optional.ofNullable(masterBroker)
+                .map(Node::getNodeDetails)
+                .map(NodeDetails::getPort)
+                .orElse(Integer.parseInt(PropertiesHelper.getProperty("master.broker.port")));
+
+        return new AbstractMap.SimpleEntry<>(masterBrokerIp, masterBrokerPort);
     }
 
     public Socket getSocket() {
