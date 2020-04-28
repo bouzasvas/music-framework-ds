4ffd6dc2b30f7be041f64a184321c612b57d0e87
Bill Bouzas <bill_bouzas@hotmail.com>
2020-04-28 23:07:27 +0300

    Create Android Splash Screen -- Check for Themes instead

---
+++ android-music-application/Lalapp/app/src/main/java/gr/aueb/ds/music/android/lalapp/SplashScreen.java
@@ -0,0 +1,34 @@
+package gr.aueb.ds.music.android.lalapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.Objects;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static final int TIMEOUT_SECS = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Remove Title Bar
+        Objects.requireNonNull(this.getSupportActionBar()).hide();
+
+        setContentView(R.layout.activity_splash_screen);
+        startTimerForMainActivity();
+    }
+
+    private void startTimerForMainActivity() {
+        // Go To main activity after X Seconds
+        new Handler().postDelayed(this::goToMainActivity, TIMEOUT_SECS);
+    }
+
+    private void goToMainActivity() {
+        Intent mainActivityIntent = new Intent(this, MainActivity.class);
+        startActivity(mainActivityIntent);
+    }
+}
