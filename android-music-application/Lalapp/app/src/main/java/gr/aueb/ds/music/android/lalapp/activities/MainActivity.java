d0a222d7473bc90da19666a9c316c2f88f9c3e5f
Panos <tassias1pan@gmail.com>
2020-05-21 23:17:07 +0300

    Implement Offline Mode

--- android-music-application/Lalapp/app/src/main/java/gr/aueb/ds/music/android/lalapp/activities/MainActivity.java
+++ android-music-application/Lalapp/app/src/main/java/gr/aueb/ds/music/android/lalapp/activities/MainActivity.java
@@ -1,7 +1,9 @@
 package gr.aueb.ds.music.android.lalapp.activities;
 
 import android.annotation.SuppressLint;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
@@ -16,11 +18,13 @@ import gr.aueb.ds.music.android.lalapp.R;
 import gr.aueb.ds.music.android.lalapp.RecyclerItemClickListener;
 import gr.aueb.ds.music.android.lalapp.RecyclerViewAdapter;
 import gr.aueb.ds.music.android.lalapp.common.AppCommon;
+import gr.aueb.ds.music.android.lalapp.common.AppFileOperations;
 import gr.aueb.ds.music.android.lalapp.helpers.LogHelper;
 import gr.aueb.ds.music.android.lalapp.helpers.NotificationsHelper;
 import gr.aueb.ds.music.android.lalapp.request.async.AsyncTaskError;
 import gr.aueb.ds.music.android.lalapp.request.async.AsyncTaskProgress;
 import gr.aueb.ds.music.android.lalapp.request.async.BrokerAsyncRequest;
+import gr.aueb.ds.music.android.lalapp.request.async.OfflineTrackAsyncRequest;
 import gr.aueb.ds.music.android.lalapp.request.async.TrackAsyncRequest;
 import gr.aueb.ds.music.framework.model.dto.ArtistName;
 import gr.aueb.ds.music.framework.model.dto.MusicFile;
@@ -37,11 +41,11 @@ public class MainActivity extends ParentActivity
 
     private Switch switchButton;
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initOnLayoutTouchListener();
 
         recyclerView = findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -51,6 +55,24 @@ public class MainActivity extends ParentActivity
         switchButton = findViewById(R.id.offline_switch);
         switchButton.setOnCheckedChangeListener((buttonView, isChecked) ->
                 switchButton.setText(switchButton.isChecked() ? getString(R.string.switch_online) : getString(R.string.switch_offline)));
+
+        retrieveDownloadedFiles(switchButton.isChecked());
+        initOnLayoutTouchListener();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void retrieveDownloadedFiles(boolean onlineMode) {
+        // When in offline mode retrieve Music Files from Device Storage
+        if (!onlineMode) {
+            try {
+                this.retrievedMusicFiles = AppFileOperations.getDownloadedFiles(MainActivity.this);
+                adapter = new RecyclerViewAdapter(this.retrievedMusicFiles);
+                recyclerView.setAdapter(adapter);
+                recyclerView.addItemDecoration(new DividerItemDecoration(getApplicationContext(), DividerItemDecoration.VERTICAL));
+            } catch (Exception e) {
+                NotificationsHelper.showToastNotification(MainActivity.this, getString(R.string.loading_local_files_failed));
+            }
+        }
     }
 
     @SuppressLint("ClickableViewAccessibility")
@@ -87,14 +109,20 @@ public class MainActivity extends ParentActivity
         }
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onItemClick(View view, int position) {
         boolean onlineMode = switchButton.isChecked();
-
         MusicFile selectedTrack = this.retrievedMusicFiles.get(position);
 
-        TrackAsyncRequest trackAsyncRequest = new TrackAsyncRequest(MainActivity.this, this.consumer, onlineMode);
-        trackAsyncRequest.execute(selectedTrack);
+        if (AppFileOperations.musicFileExists(MainActivity.this, selectedTrack)) {
+            OfflineTrackAsyncRequest offlineTrackAsyncRequest = new OfflineTrackAsyncRequest(MainActivity.this);
+            offlineTrackAsyncRequest.execute(selectedTrack);
+        }
+        else {
+            TrackAsyncRequest trackAsyncRequest = new TrackAsyncRequest(MainActivity.this, this.consumer, onlineMode);
+            trackAsyncRequest.execute(selectedTrack);
+        }
     }
 
     // AsyncTaskProgress instance
