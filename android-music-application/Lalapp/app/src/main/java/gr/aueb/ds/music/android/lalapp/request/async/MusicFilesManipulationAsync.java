6eee30efb57d3504b292b576bae1edcf0581a1ba
Bill Bouzas <bill_bouzas@hotmail.com>
2020-05-23 16:49:37 +0300

    Implemented Generic AsyncTaskWithDialog

--- android-music-application/Lalapp/app/src/main/java/gr/aueb/ds/music/android/lalapp/request/async/MusicFilesManipulationAsync.java
+++ android-music-application/Lalapp/app/src/main/java/gr/aueb/ds/music/android/lalapp/request/async/MusicFilesManipulationAsync.java
@@ -1,39 +1,17 @@
 package gr.aueb.ds.music.android.lalapp.request.async;
 
-import android.annotation.SuppressLint;
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
-import android.os.AsyncTask;
-import android.os.Environment;
 import android.util.Log;
 
-import java.io.File;
-import java.io.FileOutputStream;
-
-import gr.aueb.ds.music.android.lalapp.activities.PlayerActivity;
 import gr.aueb.ds.music.android.lalapp.R;
-import gr.aueb.ds.music.android.lalapp.common.AppCommon;
+import gr.aueb.ds.music.android.lalapp.activities.PlayerActivity;
 import gr.aueb.ds.music.android.lalapp.common.AppFileOperations;
 import gr.aueb.ds.music.android.lalapp.helpers.NotificationsHelper;
 import gr.aueb.ds.music.framework.model.dto.MusicFile;
 
-public abstract class MusicFilesManipulationAsync extends AsyncTask<MusicFile, Void, MusicFile> {
+public abstract class MusicFilesManipulationAsync extends AsyncTaskWithDialog<MusicFile, Void, MusicFile> {
 
-    @SuppressLint("StaticFieldLeak")
-    protected Context context;
-
-    @Override
-    protected void onPreExecute() {
-        super.onPreExecute();
-        AppCommon.showDialog(this.context, context.getString(R.string.dialog_loading));
-    }
-
-    @Override
-    protected void onPostExecute(MusicFile musicFile) {
-        super.onPostExecute(musicFile);
-        AppCommon.dismissDialog();
-    }
 
     public MusicFilesManipulationAsync(Context context) {
         this.context = context;
