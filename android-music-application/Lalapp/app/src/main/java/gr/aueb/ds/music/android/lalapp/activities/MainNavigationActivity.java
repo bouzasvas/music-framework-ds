b8deb36e5bc2246b4cef4db498afea833255d690
Bill Bouzas <bill_bouzas@hotmail.com>
2020-05-23 20:08:58 +0300

    Added new Downloads Menu Fragment

--- android-music-application/Lalapp/app/src/main/java/gr/aueb/ds/music/android/lalapp/activities/MainNavigationActivity.java
+++ android-music-application/Lalapp/app/src/main/java/gr/aueb/ds/music/android/lalapp/activities/MainNavigationActivity.java
@@ -28,7 +28,7 @@ public class MainNavigationActivity extends ParentActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_settings)
+                R.id.nav_home, R.id.nav_settings, R.id.nav_downloads)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
