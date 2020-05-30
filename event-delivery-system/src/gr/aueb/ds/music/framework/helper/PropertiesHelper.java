d6d59fa7d3f0c6a25a6417e8d5f0a1b1c04da6b6
Panos <tassias1pan@gmail.com>
2020-05-30 19:01:04 +0300

    Get Properties from filesystem

--- event-delivery-system/src/gr/aueb/ds/music/framework/helper/PropertiesHelper.java
+++ event-delivery-system/src/gr/aueb/ds/music/framework/helper/PropertiesHelper.java
@@ -1,8 +1,12 @@
 package gr.aueb.ds.music.framework.helper;
 
+import gr.aueb.ds.music.framework.commons.ProgramArguments;
 import gr.aueb.ds.music.framework.commons.SystemExitCodes;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.Properties;
 
 public class PropertiesHelper {
@@ -15,9 +19,34 @@ public class PropertiesHelper {
     }
 
     private static void loadProperties() {
+        boolean sameLocationAsJar = false;
+
         props = new Properties();
         try {
-            props.load(ClassLoader.getSystemResourceAsStream("app.properties"));
+            sameLocationAsJar = !ProgramArguments.getArgument("--property-file-jar-dir").isEmpty();
+
+            final String classPathProperties = ProgramArguments.getArgument("--property-file-dir").get(0);
+            PropertiesHelper.loadProperties(classPathProperties, sameLocationAsJar);
+        }
+        catch (IndexOutOfBoundsException ex) {
+            PropertiesHelper.loadProperties(null, sameLocationAsJar);
+        }
+
+    }
+
+    private static void loadProperties(String dir, boolean sameLocationAsJar) {
+        try {
+            if (sameLocationAsJar) {
+                dir = new File("").getAbsolutePath();
+            }
+
+            if (dir == null) {
+                props.load(ClassLoader.getSystemResourceAsStream("app.properties"));
+            }
+            else {
+                System.out.println(dir);
+                props.load(new FileInputStream(dir + "\\app.properties"));
+            }
         }
         catch (IOException ex) {
             System.err.println("Could not load Properties file app.properties");
